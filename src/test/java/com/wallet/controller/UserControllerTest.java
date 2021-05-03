f72e74e47f85734fdaa225bdb0adf8e6ecc040e2
Anderson Monte <andersonxef@gmail.com>
2021-05-03 09:24:08 -0300

    Criado UserController e seus testes

--- src/test/java/com/wallet/controller/UserControllerTest.java
+++ src/test/java/com/wallet/controller/UserControllerTest.java
@@ -2,7 +2,10 @@ package com.wallet.controller;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wallet.dto.UserDTO;
 import com.wallet.entity.User;
 import com.wallet.service.UserService;
 
@@ -34,7 +39,11 @@ public class UserControllerTest {
 	@Autowired
 	MockMvc mvc;
 	
+	@Test
 	public void testSave() throws Exception {
+		
+		BDDMockito.given(service.save(Mockito.any(User.class))).willReturn(getMockUser());
+		
 		mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayload())
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON))
@@ -50,7 +59,7 @@ public class UserControllerTest {
 		return u;
 	}
 	
-	public String getJsonPayload() {
+	public String getJsonPayload() throws JsonProcessingException {
 		UserDTO dto = new UserDTO();
 		dto.setEmail(EMAIL);
 		dto.setName(NAME);
