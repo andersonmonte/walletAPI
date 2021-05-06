ec8cb5da9ef0b15e66145f7bf0b2cb35b4f7c2de
Anderson Monte <andersonxef@gmail.com>
2021-05-05 22:21:54 -0300

    Banco de dados, Flyway, Postman, criptografia e ocultação de senha configurados.

--- src/main/resources/db/migration/postgresql/V1__init.sql
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -1,4 +1,4 @@
-create table users (
+create table users(
 id serial,
 name varchar(50),
 password varchar,
@@ -11,3 +11,20 @@ name varchar(60),
 value numeric(10,2),
 primary key (id));
 
+create table users_wallet(
+id serial,
+wallet integer,
+users integer,
+primary key(id),
+foreign key(users) references users(id),
+foreign key(wallet) references wallet(id));
+
+create table wallet_items(
+id serial,
+wallet integer,
+date date,
+type varchar(2),
+description varchar(500),
+value numeric(10,2),
+primary key(id),
+foreign key(wallet) references wallet(id));
